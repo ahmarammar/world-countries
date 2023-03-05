53bfd9306dedc5ddc7f949eece7264861a953593
Ahmar Ammar <211370160@gift.edu.pk>
2023-03-05 10:53:13 +0500

    added a minor animation for the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router'
 import CountryPage from './components/CountryPage'
 import Header from './components/Header'
@@ -6,14 +5,17 @@ import Homepage from './components/Homepage'
 import data from "./data/data.json"
 
 const App = () => {
+  
   return (
-    <Routes>
-      <Route path='/' element={<Homepage />} />
-      <Route path=':countryID' element={<>
-        <Header />
-        <CountryPage data={data} />
-      </>} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/:countryID' element={<>
+          <CountryPage data={data} />
+        </>} />
+      </Routes>
+    </>
   )
 }
 
